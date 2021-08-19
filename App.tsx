8365b10cb1cefe47706bb06336b2a5c02da66230
Arlin-Mafra <arlinsantana@gmail.com>
2021-08-19 10:25:13 -0300

    global fonts

--- App.tsx
+++ App.tsx
@@ -2,9 +2,26 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { ThemeProvider } from "styled-components";
 import theme from "./src/global/styles/theme";
+import AppLoading from "expo-app-loading";
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+} from "@expo-google-fonts/poppins";
 import { DashBoard } from "./src/screens";
 
 export default function App() {
+  const [fontsLoading] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  });
+
+  if (!fontsLoading) {
+    return <AppLoading />;
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <DashBoard />
--- src/screens/styles.ts
+++ src/screens/styles.ts
@@ -5,11 +5,11 @@ export const Container =  styled.View`
     flex: 1;
     justify-content: center;
     align-items: center;
-    background-color: ${({theme}) => theme.colors.background}
+    background-color: ${({theme}) => theme.colors.background};
 `;
 
 export const Title = styled.Text`
+    font-family: ${({theme}) => theme.fonts.bold };
     font-size: 24px;
-    font-weight: bold;
     color: ${({theme}) => theme.colors.title};
 `;
